526898289aeced70703cc454eb5835464f41004e
Simon Graham <simon_graham@hotmail.co.uk>
2017-08-10 10:20:28 +0100

    Update find_tp_fp_fn.m

--- find_tp_fp_fn.m
+++ find_tp_fp_fn.m
@@ -1,8 +1,19 @@
 function[tp, fp, fn] = find_tp_fp_fn(mask, gt)
-% parameters 
-patchW =  80;
-patchH = 80;
-thres = 0.5; % means 50%
+
+% Author: Simon Graham
+% Tissue Image Analytics Lab
+% Department of Computer Science, 
+% University of Warwick, UK.
+%-------------------------------------------------------------------
+% Parameters 
+patchW =  80; % Width of patch
+patchH = 80; % Height of patch
+threshold = 0.5; % 50% threshold
+
+% Arguments
+% mask: predicted binary image
+% gt: ground truth binary image
+%-------------------------------------------------------------------
 
 % Regions for TP, FP, FN
 tp_mask =  im2bw((mask & gt));
@@ -15,13 +26,13 @@ for i=0:floor(size(gt,1)/patchH)-1
         tp_patch =  tp_mask(i*patchH +1 :(i+1)*patchH, j*patchW+1:(j+1)*patchW );
         fp_patch = fp_mask(i*patchH + 1:(i+1)*patchH, j*patchW+1:(j+1)*patchW );
         fn_patch = fn_mask(i*patchH+1:(i+1)*patchH, j*patchW+1:(j+1)*patchW );
-        if (mean(tp_patch(:)) >= 0.5)
+        if (mean(tp_patch(:)) >= threshold)
             tp =  tp + 1;
         end
-        if (mean(fp_patch(:)) >= 0.5)
+        if (mean(fp_patch(:)) >= threshold)
             fp =  fp + 1;
         end
-        if (mean(fn_patch(:)) >= 0.5)
+        if (mean(fn_patch(:)) >= threshold)
             fn =  fn + 1;
         end
       
